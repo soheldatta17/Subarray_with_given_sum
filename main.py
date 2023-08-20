2eb2874c3eb41e09226afe724135c300c5b8ac4b
soheldatta17 <soheldatta17@gmail.com>
2023-08-20 12:21:20 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subArraySum(self,arr, n, s):
+        if s in arr:
+            b=arr.index(s)
+            return [b+1,b+1]
+	    sum = arr[0]
+	    start = 0
+	    i = 1
+	    while i <= n:
+	        while sum > s and start < i-1:
+	            sum -= arr[start]
+	            start += 1
+		    if sum == s:
+		        return [start+1,i]
+		    if i < n:
+		        sum = sum + arr[i]
+		    i += 1
+	    return [-1]
